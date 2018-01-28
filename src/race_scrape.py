e2b0f4e8f9f528ee2a38a955531806b6ebba3fbd
gabrielx52 <gabriel.meringolo@gmail.com>
2018-01-27 16:51:23 -0800

    Got all URLS, made dict with them.

--- src/race_scrape.py
+++ src/race_scrape.py
@@ -11,8 +11,7 @@ import requests
 def date_range_1991_to_2017():
     """Generate all dates from 1991 to 2017."""
     # start_date = date(1991, 1, 1)
-    # start_date = date(1997, 10, 20)
-    start_date = date(2005, 4, 8)
+    start_date = date(2015, 1, 10)
 
     end_date = date(2018, 1, 1)
     for n in range(int((end_date - start_date).days)):
@@ -34,7 +33,7 @@ def first_parser():  # pragma: no cover
     count = 1
     with open('./first_urls.txt', 'a+') as f:
         for url in historical_url_gen():
-            print(round(100 * float(count) / float(9886), 2))
+            print(round(100 * float(count) / float(1114), 2))
             count += 1
             r = requests.get(url, headers={'User-agent': 'Bruce Pfiffer'})
             while r.status_code != 200:
@@ -57,3 +56,23 @@ def second_parser():
             r = requests.get(url, headers={'User-agent': 'Wayne Mazerati'})
         s = Soup(r.content, 'html.parser').find('a', class_='dkbluesm').attrs['href']
         yield root_url.format(s)
+
+
+def race_dict_maker():
+    """Populate dictonary with race info from urls."""
+    ret_d = {}
+    with open('first_urls.txt', 'r') as f:
+        for line in f:
+            _, tc, dt, cc = line.split('=')
+            tc = tc.split('&')[0]
+            dt = dt.split('&')[0]
+            cc = cc.strip()
+            ret_d.setdefault(tc + '|' + cc, []).append(dt)
+    with open('race_dates.txt', 'a') as f:
+        f.write(str(ret_d))
+
+
+def race_data_reader():
+    """Parse race data dict and extract info."""
+    from race_dict import race_dict as rd
+    return list(rd.keys())
